9dc6dcb443715f91ec5263ce77707213b5c29a3f
Gabriellji <gabriellji@yahoo.com>
2020-05-04 00:07:04 +0300

    feat: distinct fn completed

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -489,8 +489,9 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const filtered = arr.filter((x, idx, array) => array.indexOf(x) === idx);
+  return filtered;
 }
 
 /**
